b4524576b1841d4f0ae4998ffbaf5960cfd4cbe0
AbialiDr <abiali.doctor@propelius.tech>
2026-02-26 23:52:01 +0530

    feat: Add new styling modules for reviews, authentication, student, leads, planning, makeup, and dashboard components, along with a new config file.

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-export const PROXY = "http://35.154.179.196:8080";
+export const PROXY = "http://localhost:8080";
 export const S3PROXY = "https://wedfield-prod.s3.ap-south-1.amazonaws.com";
 
 export const firebaseUrl =
